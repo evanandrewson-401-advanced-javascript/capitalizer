b104c5ee3c7352e8059093662c2685fd477c31f2
evanandrewson <evanandrewson@gmail.com>
2019-10-09 14:29:39 -0700

    created write-file event and listener

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ io.on('connection', socket => {
   socket.on('file-read', (path, data) => {
     socket.broadcast.emit('file-read', path, data);
   });
-  socket.on('file-write', () => {});
+  socket.on('file-write', data => {
+    socket.broadcast.emit('file-write', path, data)
+  });
   socket.on('file-saved', () => {});
   socket.on('file-error', () => {});
 });
