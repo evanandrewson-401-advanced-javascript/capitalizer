dce5891a0bc6a8123f609401322f84dcd8ff7987
evanandrewson <evanandrewson@gmail.com>
2019-10-09 11:39:53 -0700

    completed setup and skeleton of server.js listeners

---
+++ server.js
@@ -0,0 +1,8 @@
+const io = require('socket.io')(7890);
+
+io.on('connection', socket => {
+  socket.on('file-read', () => {});
+  socket.on('file-write', () => {});
+  socket.on('file-saved', () => {});
+  socket.on('file-error', () => {});
+})
